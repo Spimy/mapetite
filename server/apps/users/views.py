13ecf29c6215776322b4704200a91070290ee5b9
William <williamlaw.3001@gmail.com>
2026-05-17 22:54:31 +0800

    #1 add sign in view

--- server/apps/users/views.py
+++ server/apps/users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic import FormView
+from django.contrib.auth import login
+from .forms import SignInForm
+from .mixins import SuccessUrlMixin
+
 
 # Create your views here.
+class SignInView(SuccessUrlMixin, FormView):
+    template_name = "users/signin.html"
+    form_class = SignInForm
+    success_url = "/admin/"  # TODO: This should be linked to the merchant dashboard once it's implemented
+
+    def form_valid(self, form):
+        login(self.request, form.get_user())
+        return super().form_valid(form)
