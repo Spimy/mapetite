0219466910d1b58cd686541c344963befea17453
William <williamlaw.3001@gmail.com>
2026-05-17 23:09:25 +0800

    #1 add signout view

--- server/apps/users/views.py
+++ server/apps/users/views.py
@@ -1,7 +1,10 @@
 from django.views.generic import FormView
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from .forms import SignInForm
 from .mixins import SuccessUrlMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.base import RedirectView
+from django.urls import reverse_lazy
 
 
 # Create your views here.
@@ -13,3 +16,12 @@ class SignInView(SuccessUrlMixin, FormView):
     def form_valid(self, form):
         login(self.request, form.get_user())
         return super().form_valid(form)
+
+
+class SignOutView(LoginRequiredMixin, RedirectView):
+    url = reverse_lazy("users:sign-in")  # TODO: To update to marketing/landing page
+    redirect_field_name = None
+
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        return super(SignOutView, self).get(request, *args, **kwargs)
