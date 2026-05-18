0276d871b729149437389dcee7ab8cca3c15a352
William <williamlaw.3001@gmail.com>
2026-05-18 20:30:27 +0800

    #1 add api routes to get jwt tokens

--- server/apps/users/urls.py
+++ server/apps/users/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 app_name = "users"
 
@@ -7,4 +12,7 @@ app_name = "users"
 urlpatterns = [
     path("sign-in/", views.SignInView.as_view(), name="sign_in"),
     path("sign-out/", views.SignOutView.as_view(), name="sign_out"),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
 ]
