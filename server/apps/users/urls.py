9890e22ef85dc4e7e608e1e2ad18f396553e4ce4
William <williamlaw.3001@gmail.com>
2026-05-18 22:53:26 +0800

    #1 add urls for resetting password

--- server/apps/users/urls.py
+++ server/apps/users/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
+from django.contrib.auth import views as auth_views
 from . import views
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -12,6 +13,31 @@ app_name = "users"
 urlpatterns = [
     path("sign-in/", views.SignInView.as_view(), name="sign_in"),
     path("sign-out/", views.SignOutView.as_view(), name="sign_out"),
+    path(
+        "reset-password/",
+        auth_views.PasswordResetView.as_view(
+            success_url=reverse_lazy("users:password_reset_done"),
+            email_template_name="users/password_reset_email.txt",
+        ),
+        name="password_reset",
+    ),
+    path(
+        "reset-password/sent/",
+        auth_views.PasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+    path(
+        "reset-password/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            success_url=reverse_lazy("users:password_reset_complete")
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset-password/complete/",
+        auth_views.PasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+    ),
     path("api/auth/google/", views.GoogleLoginView.as_view(), name="google_login"),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
