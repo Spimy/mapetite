b23987a8a60fb50eb539db288f7158d4b3c513fd
William <williamlaw.3001@gmail.com>
2026-05-19 09:10:38 +0800

    #1 add email verification view

--- server/apps/users/views.py
+++ server/apps/users/views.py
@@ -1,4 +1,6 @@
 from django.views.generic import FormView
+from django.views import View
+from django.shortcuts import render
 from django.contrib.auth import login, logout
 from .forms import SignInForm
 from .mixins import SuccessUrlMixin
@@ -7,13 +9,15 @@ from django.views.generic.base import RedirectView
 from django.urls import reverse_lazy
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from dj_rest_auth.registration.views import SocialLoginView
+from allauth.account.models import EmailConfirmationHMAC, EmailConfirmation
 
 
 # Create your views here.
 class SignInView(SuccessUrlMixin, FormView):
     template_name = "users/signin.html"
     form_class = SignInForm
-    success_url = "/admin/"  # TODO: This should be linked to the merchant dashboard once it's implemented
+    # TODO: This should be linked to the merchant dashboard once it's implemented
+    success_url = "/admin/"
 
     def form_valid(self, form):
         login(self.request, form.get_user())
@@ -21,7 +25,8 @@ class SignInView(SuccessUrlMixin, FormView):
 
 
 class SignOutView(LoginRequiredMixin, RedirectView):
-    url = reverse_lazy("users:sign_in")  # TODO: To update to marketing/landing page
+    # TODO: To update to marketing/landing page
+    url = reverse_lazy("users:sign_in")
     redirect_field_name = None
 
     def get(self, request, *args, **kwargs):
@@ -33,3 +38,24 @@ class GoogleLoginView(SocialLoginView):
     """Takes the access token from the frontend provided by Google and uses it to log in the user via Google OAuth2"""
 
     adapter_class = GoogleOAuth2Adapter
+
+
+class ConfirmEmailView(View):
+    def get(self, request, key, *args, **kwargs):
+        confirmation = self.get_confirmation(key)
+
+        if confirmation:
+            confirmation.confirm(self.request)
+            return render(request, "users/email_verification_success.html")
+        else:
+            return render(request, "users/email_verification_failed.html")
+
+    def get_confirmation(self, key):
+        """Helper method to extract the confirmation object from the key"""
+        try:
+            return EmailConfirmationHMAC.from_key(key)
+        except Exception:
+            try:
+                return EmailConfirmation.objects.get(key=key.lower())
+            except EmailConfirmation.DoesNotExist:
+                return None
