4b50557123fa5728defabc9d1a2d825b8b4b9833
William <williamlaw.3001@gmail.com>
2026-05-21 11:38:30 +0800

    #3 add api endpoint to get list of stores

--- server/apps/merchants/views.py
+++ server/apps/merchants/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import StoreProfile
+from .serializers import StoreProfileSerializer
+from rest_framework.permissions import IsAuthenticated
+
 
 # Create your views here.
+class StoreListAPIView(generics.ListAPIView):
+    """Fetches all stores and their grouped operating hours"""
+
+    queryset = StoreProfile.objects.prefetch_related("operating_hours").all()
+    serializer_class = StoreProfileSerializer
+    permission_classes = [IsAuthenticated]
