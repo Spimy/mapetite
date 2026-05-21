ce4192740d4fe006eb7db284f98334c0f74b6408
William <williamlaw.3001@gmail.com>
2026-05-21 11:53:17 +0800

    #3 add api endpoints for specific store

--- server/apps/merchants/urls.py
+++ server/apps/merchants/urls.py
@@ -5,4 +5,10 @@ app_name = "merchants"
 
 urlpatterns = [
     path("api/stores/", view=views.StoreListAPIView.as_view(), name="store_list"),
+    path("api/stores/<int:store_id>/", view=views.StoreAPIView.as_view(), name="store"),
+    path(
+        "api/stores/<int:store_id>/operating-hours/",
+        view=views.StoreOperatingHoursAPIView.as_view(),
+        name="store_operating_hours",
+    ),
 ]
--- server/apps/merchants/views.py
+++ server/apps/merchants/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
-from .models import StoreProfile
-from .serializers import StoreProfileSerializer
+from .models import StoreOperatingHour, StoreProfile
+from .serializers import StoreOperatingHourSerializer, StoreProfileSerializer
 from rest_framework.permissions import IsAuthenticated
 
 
@@ -11,3 +11,23 @@ class StoreListAPIView(generics.ListAPIView):
     queryset = StoreProfile.objects.prefetch_related("operating_hours").all()
     serializer_class = StoreProfileSerializer
     permission_classes = [IsAuthenticated]
+
+
+class StoreAPIView(generics.RetrieveAPIView):
+    """Fetches a single store and its grouped operating hours"""
+
+    queryset = StoreProfile.objects.prefetch_related("operating_hours").all()
+    serializer_class = StoreProfileSerializer
+    permission_classes = [IsAuthenticated]
+    lookup_field = "id"
+    lookup_url_kwarg = "store_id"
+
+
+class StoreOperatingHoursAPIView(generics.ListAPIView):
+    """Fetches only the operating hours for a store"""
+
+    queryset = StoreOperatingHour.objects.all()
+    serializer_class = StoreOperatingHourSerializer
+    permission_classes = [IsAuthenticated]
+    lookup_field = "store_id"
+    lookup_url_kwarg = "store_id"
