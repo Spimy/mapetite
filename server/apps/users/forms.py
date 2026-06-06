4ecffcc769e1ccc753e1ef1efd83e99f18eb1da2
William <williamlaw.3001@gmail.com>
2026-06-06 13:39:08 +0000

    #7 add user info form

--- server/apps/users/forms.py
+++ server/apps/users/forms.py
@@ -22,3 +22,29 @@ class SignInForm(AuthenticationForm):
         label="Password",
         widget=forms.widgets.PasswordInput(attrs={"placeholder": "••••••••"}),
     )
+
+
+class UserInfoForm(forms.ModelForm):
+    owner_name = forms.CharField(label="Owner Name", required=False)
+    email = forms.EmailField(label="Email", required=False)
+
+    class Meta:
+        model = UserProfile
+        fields = ["avatar", "phone_number", "address", "city", "country"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        user = self.instance.user
+        full_name = f"{user.first_name} {user.last_name}".strip()
+        
+        self.fields['owner_name'].initial = full_name if full_name else user.username
+        self.fields['owner_name'].widget.attrs['readonly'] = True
+        self.fields['owner_name'].widget.attrs['disabled'] = True
+
+        self.fields['email'].initial = user.email
+        self.fields['email'].widget.attrs['readonly'] = True
+        self.fields['email'].widget.attrs['disabled'] = True
+
+    def save(self, commit=True):
+        return super().save(commit=commit)
