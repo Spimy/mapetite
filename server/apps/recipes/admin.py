83991d29d17a1e3dc37fb9f97fa74fa8f8c24710
William <williamlaw.3001@gmail.com>
2026-07-10 17:05:50 +0000

    #8 add admin views for recipe models

--- server/apps/recipes/admin.py
+++ server/apps/recipes/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
+from apps.recipes.models import Recipe, RecipeIngredient, RecipeStep, SavedRecipe
+
+
 # Register your models here.
+class RecipeAdmin(admin.ModelAdmin):
+    readonly_fields = ("created_at", "updated_at")
+
+
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    readonly_fields = ("created_at", "updated_at")
+
+
+class RecipeStepAdmin(admin.ModelAdmin):
+    readonly_fields = ("created_at", "updated_at")
+
+
+class SavedRecipeAdmin(admin.ModelAdmin):
+    readonly_fields = ("saved_at",)
+
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
+admin.site.register(RecipeStep, RecipeStepAdmin)
+admin.site.register(SavedRecipe, SavedRecipeAdmin)
