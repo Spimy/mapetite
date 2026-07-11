e4a777f40cf5fd41971ed2fc372fc3d66efe5c35
Rohan919477 <rohankaghan1@gmail.com>
2026-07-12 05:09:42 +0800

    #44 revert backend authentication change
    Reverted backend changes

--- server/apps/users/backends.py
+++ server/apps/users/backends.py
@@ -1,34 +1,30 @@
+from django.db.models import Q
 from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend
-from django.db.models import Q
 
 
 class EmailOrUsernameBackend(ModelBackend):
     """
-    Allow active users to authenticate using a case-insensitive username
-    or email address.
+    Allow users to authenticate using case insensitive username
+    or using email which provides a way for users who forgot their
+    username to authenticate themselves
     """
 
     def authenticate(self, request, username=None, password=None, **kwargs):
+
         user_model = get_user_model()
 
-        identifier = username or kwargs.get(user_model.USERNAME_FIELD)
+        if username is None:
+            username = kwargs.get(user_model.USERNAME_FIELD)
 
-        if not identifier or password is None:
-            return None
-
-        username_lookup = f"{user_model.USERNAME_FIELD}__iexact"
+        case_insensitive_username_field = f"{user_model.USERNAME_FIELD}__iexact"
 
         try:
             user = user_model.objects.get(
-                Q(**{username_lookup: identifier}) |
-                Q(email__iexact=identifier)
+                Q(**{case_insensitive_username_field: username})
+                | Q(email__iexact=username)
             )
         except user_model.DoesNotExist:
-            user_model().set_password(password)
             return None
-
-        if user.check_password(password) and self.user_can_authenticate(user):
+        else:
             return user
-
-        return None
