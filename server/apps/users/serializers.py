12033c74a50882722b3eb76ff28fe9699a80677e
William <williamlaw.3001@gmail.com>
2026-07-18 17:04:09 +0000

    #74 include new fields in user details serializer

--- server/apps/users/serializers.py
+++ server/apps/users/serializers.py
@@ -1,17 +1,31 @@
 from rest_framework import serializers
+from drf_spectacular.utils import extend_schema_field
 from allauth.account.models import EmailAddress
 from .models import User, UserProfile
+
+
+class ChoiceOptionSerializer(serializers.Serializer):
+    value = serializers.CharField()
+    label = serializers.CharField()
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
         fields = [
+            "onboarding_completed",
             "avatar",
+            "phone_number",
             "address",
             "city",
             "country",
-            "budget_monthly",
+            "target_calories",
+            "dine_in_budget",
+            "grocery_budget",
+            "spending_alert_percent",
+            "health_goal",
+            "activity_level",
+            "current_weight",
             "is_halal",
             "is_vegan",
             "allergies",
@@ -23,6 +37,9 @@ class UserDetailSerializer(serializers.ModelSerializer):
 
     is_verified = serializers.SerializerMethodField()
     profile = UserProfileSerializer(source="user_profile", read_only=True)
+    allergy_options = serializers.SerializerMethodField()
+    health_goal_options = serializers.SerializerMethodField()
+    activity_level_options = serializers.SerializerMethodField()
 
     class Meta:
         model = User
@@ -34,8 +51,32 @@ class UserDetailSerializer(serializers.ModelSerializer):
             "last_name",
             "is_verified",
             "profile",
+            "allergy_options",
+            "health_goal_options",
+            "activity_level_options",
         ]
 
     def get_is_verified(self, obj):
         # Checks if the user has any verified email addresses
         return EmailAddress.objects.filter(user=obj, verified=True).exists()
+
+    @extend_schema_field(ChoiceOptionSerializer(many=True))
+    def get_allergy_options(self, obj):
+        return [
+            {"value": value, "label": label}
+            for value, label in UserProfile.AllergyChoices.choices
+        ]
+
+    @extend_schema_field(ChoiceOptionSerializer(many=True))
+    def get_health_goal_options(self, obj):
+        return [
+            {"value": value, "label": label}
+            for value, label in UserProfile.HealthGoalChoices.choices
+        ]
+
+    @extend_schema_field(ChoiceOptionSerializer(many=True))
+    def get_activity_level_options(self, obj):
+        return [
+            {"value": value, "label": label}
+            for value, label in UserProfile.ActivityLevelChoices.choices
+        ]
