fefe42e6a24685f7e75e18c2085e67bd1727ddf7
William <williamlaw.3001@gmail.com>
2026-07-30 13:06:53 +0000

    #115 add solutions page

--- server/apps/marketing/urls.py
+++ server/apps/marketing/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import LandingPageView
+from .views import LandingPageView, SolutionsPageView
 
 app_name = "marketing"
 
 urlpatterns = [
     path("", LandingPageView.as_view(), name="landing-page"),
+    path("solutions/", SolutionsPageView.as_view(), name="solutions-page"),
 ]
--- server/apps/marketing/views.py
+++ server/apps/marketing/views.py
@@ -4,3 +4,7 @@ from django.views.generic import TemplateView
 # Create your views here.
 class LandingPageView(TemplateView):
     template_name = "marketing/pages/landing.html"
+
+
+class SolutionsPageView(TemplateView):
+    template_name = "marketing/pages/solutions.html"
